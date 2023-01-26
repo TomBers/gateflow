d7a0ce88a8b0081944bb9879bf14848c1cfef65e
TomBers <tjcberman@gmail.com>
2023-01-26 11:00:25 +0000

    first tree call

--- lib/gateflow/build_tree.ex
+++ lib/gateflow/build_tree.ex
@@ -2,43 +2,55 @@ defmodule BuildTree do
   alias Gateflow.ReadResources
 
   def run do
-    board = ReadResources.get_board("156f7c2e-59ea-4aaa-bd97-af98919a7bba")
+    board = ReadResources.get_board("1cd52d70-0db4-4c06-91b5-e9e2387b95b3")
     items = board.flow_items
 
-    # _leaf_nodes = get_leaf_nodes(items)
+    # leaf_nodes = get_leaf_nodes(items)
 
-    roots = get_root_items(items)
+    grouped_by_parent =
+      items
+      |> Enum.map(fn item ->
+        %{
+          parent_id: item.flow_item_id,
+          id: item.id,
+          name: item.title,
+          is_root: item.is_root
+        }
+      end)
+      |> Enum.group_by(& &1.parent_id)
 
-    search_in_order(roots, items, [])
+    # IO.inspect(grouped_by_parent)
+
+    grouped_by_parent
+    |> Map.keys()
+    |> build_tree(grouped_by_parent, %{})
   end
 
-  def search_in_order(to_search, _items, acc) when length(to_search) == 0 do
-    acc
+  def build_tree([], _all, res) do
+    IO.inspect("Exit")
+    res
   end
 
-  def search_in_order([h | rest], items, acc) do
-    acc = acc ++ [h.title]
-    nodes = rest ++ get_children(h, items)
-    search_in_order(nodes, items, acc)
+  def build_tree([h | to_add], all, res) do
+    children = Map.get(all, h)
+
+    child_map =
+      children
+      |> Enum.map(fn item -> Map.put_new(item, :children, get_children(item, all)) end)
+      |> List.first()
+
+    build_tree(to_add, all, Map.merge(child_map, res))
   end
 
-  def add_nodes(_node, acc, _items, []) do
-    acc
-  end
+  # def search_in_order(to_search, _items, acc) when length(to_search) == 0 do
+  #   acc
+  # end
 
-  def add_nodes(node, acc, items, children) do
-    # acc =
-    Map.put_new(acc, :name, node.title)
-    |> Map.put_new(
-      :children,
-      Enum.map(children, &%{name: &1.title, children: get_children(&1, items)})
-    )
-
-    # TODO rewrite as a recursive function to make sure we go down multiple levels.
-
-    # children
-    # |> Enum.map(fn child -> add_nodes(child, acc, items, get_children(child, items)) end)
-  end
+  # def search_in_order([h | rest], items, acc) do
+  #   acc = acc ++ [h.title]
+  #   nodes = rest ++ get_children(h, items)
+  #   search_in_order(nodes, items, acc)
+  # end
 
   def get_root_items(items) do
     items |> Enum.filter(& &1.is_root)
@@ -49,6 +61,11 @@ defmodule BuildTree do
   end
 
   def get_children(item, items) do
-    items |> Enum.filter(&(&1.flow_item_id == item.id))
+    Map.get(items, item.id)
+    # items |> Enum.filter(&(&1.parent_id == item.id))
+  end
+
+  def get_item_by_id(id, items) do
+    Enum.find(items, &(&1.id == id))
   end
 end
